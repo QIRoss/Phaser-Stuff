459cc8bd6d07f2ad37762c3bd801bc054687385f
qiross <lucasqreis@poli.ufrj.br>
2021-08-16 08:42:07 -0300

    luiszuno up to 6

--- luiszuno/game.js
+++ luiszuno/game.js
@@ -1,8 +1,15 @@
 const config = {
-    width: 800,
-    height: 600,
+    width: 256,
+    height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug: false
+        }
+    }
 }
-
+  
 const game = new Phaser.Game(config);
